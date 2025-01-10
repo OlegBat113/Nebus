facfd89360c47e10c292b8074a6e0b55870bad80
OlegBat113 <oleg.batomunkuev2@gmail.com>
2025-01-11 01:50:48 +0800

    Update schemas.py

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,13 +1,34 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
+# Схема для зданий
+class BuildingSchema(BaseModel):
+    id: int
+    address: str
+    latitude: str
+    longitude: str
+    class Config:
+        orm_mode = True
+
+# Схема для деятельности
+class ActivitySchema(BaseModel):
+    id: int
+    name: str
+    parent_id: Optional[int] = None
+    class Config:
+        orm_mode = True
+
+# Схема для организаций
 class OrganizationSchema(BaseModel):
     id: int
     name: str
-    phone_numbers: Optional[str] = None
-
+    phone_numbers: Optional[List[str]] = None
+    activity: Optional[List[ActivitySchema]] = None
+    building: Optional[List[BuildingSchema]] = None
     class Config:
         orm_mode = True
+
+
 
 # Если у вас есть другие схемы, добавьте их здесь
 
