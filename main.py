22bf50273d6ddc7cb094cc04a472f50c27b84668
OlegBat113 <oleg.batomunkuev2@gmail.com>
2025-01-11 12:09:40 +0800

    templates

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+import fastapi
+#from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import fastapi.templating
+
 from app.routes.router import router
 from app.database.database import engine, Base
 import uvicorn
@@ -10,11 +16,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Настройка статических файлов
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Настройка шаблонов Jinja2
+templates = fastapi.templating.Jinja2Templates(directory="templates")
+
 app.include_router(router)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
